81b8509e501701cc7bf420f7cf6585b8a6c5a8de
KonstantinKH <kkhomenko9@gmail.com git config --global user.nameKonstantin>
2017-07-29 11:51:29 +0300

    adding snipets

---
+++ snipets.js
@@ -0,0 +1,102 @@
+
+
+	/*  menu links  */
+
+
+	$('.to-why').click(function(e){
+	e.preventDefault();
+	$('body, html').animate({ scrollTop: $('.s-we').offset().top }, 1000);
+	}); 
+
+
+	/*  menu links  end  */
+
+
+	/*   make a Grid  */
+
+	var $window = $(window);
+
+	makeGrid();
+
+	$window.resize(function(){
+		
+		makeGrid();
+
+	}); //resize
+
+
+	function makeGrid(){
+
+		if(($window).width() < 640 ){
+
+		$(".we-item__grid").removeClass("col-xs-4").addClass("col-xs-12");
+		
+		}else{
+
+		$(".we-item__grid").removeClass("col-xs-12").addClass("col-xs-4");	
+
+		}
+
+		if(($window).width() < 480 ){
+
+		$(".after-print__item").removeClass("col-xs-6").addClass("col-xs-12");
+
+		}else{
+
+		$(".after-print__item").removeClass("col-xs-12").addClass("col-xs-6");
+
+		}
+
+
+	}; //makeGrid
+
+
+	/*   make a Grid   end  */
+
+
+
+
+	/*   waypoint */
+
+
+
+	$("h2").each(function(){
+
+		var animH2 = $(this);
+
+		$(this).waypoint(function() {
+			
+			animH2.addClass("h2_animat");
+
+		}, {
+			offset : "70%"
+		});
+
+	});
+
+
+	$(".we-item img").waypoint(function() {
+	
+	$(".we-item img").addClass("we-item_animat");
+
+	}, {
+		offset : "60%"
+	});
+
+
+
+	$(".s-surface").waypoint(function() {
+	$(".s-surface li").each(function(index) {
+		var ths = $(this);
+		setInterval(function() {
+			ths.addClass("s-surface__animat");
+		}, 400*index);
+	});
+	}, {
+		offset : "80%"
+	});
+
+
+
+
+	/*   waypoint end*/
